a65eb41cca268946e7473bb64f7cd40ff20a2b38
nafi-rahman <nafirahman16@gmail.com>
2022-03-13 00:45:54 +0600

    secontime

--- Virtual_Meet/resources/views/layouts/logReg.blade.php
+++ Virtual_Meet/resources/views/layouts/logReg.blade.php
@@ -4,9 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<<<<<<< HEAD
     <link rel="stylesheet" href="{{ URL::asset('css/logReg.css'); }}" crossorigin="anonymous">
->>>>>>> d8a8d4428eaae59f441bc6c1adcb5d73a6c022bc
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>Document</title>
 </head>
@@ -15,12 +13,11 @@
     justify-content: center; padding: 50px 50px;">
     <div class="container">
         <div class="row">
-            <div class="col" style="background-color: blue">
+            <div class="col-md-6" style="background-color: blue">
                 @yield('left-content')
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque voluptas quaerat pariatur ad tempora nihil quidem. Dignissimos, adipisci? Id, architecto sapiente? Atque quam animi maiores repellendus quidem. Nostrum, ratione asperiores?</p>
             </div>
-            <div class="col" style="background-color: red">
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque voluptas quaerat pariatur ad tempora nihil quidem. Dignissimos, adipisci? Id, architecto sapiente? Atque quam animi maiores repellendus quidem. Nostrum, ratione asperiores?</p>
+            <div class="col-md-6" style="background-color: red">
+
                 @yield('right-content')
             </div>
         </div>
@@ -28,14 +25,10 @@
         <div class="row">
             <div class="col" style="background-color: green">
                 @yield('bottom-content')
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque voluptas quaerat pariatur ad tempora nihil quidem. Dignissimos, adipisci? Id, architecto sapiente? Atque quam animi maiores repellendus quidem. Nostrum, ratione asperiores?</p>
+
 
             </div>
     </div>
-<<<<<<< HEAD
-=======
-    <p>hello</p>
->>>>>>> d8a8d4428eaae59f441bc6c1adcb5d73a6c022bc
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
--- Virtual_Meet/resources/views/pages/login/login.blade.php
+++ Virtual_Meet/resources/views/pages/login/login.blade.php
@@ -1,10 +1,7 @@
-<html>
-    <head>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+@extends('layouts.logReg')
+    @section('left-content')
 
-    </head>
 
-    <body>
     <div class = "container">
     <br><br>
     <h1>Login Form</h1>
@@ -30,11 +27,11 @@
   			<label for="rememberMe">Remember Me</label>
         </div>
         <br>
+        @endsection
+        @section('bottom-content')
         <input type = "submit" class="btn btn-primary" value = "Login">
         Don't have an account? <a href="{{route('registration')}}">sign-up</a>
         <br><br>
         <a href="{{ route('homeUser') }}">Continue as guest</a>
+        @endsection
     </form>
-    </div>
-    </body>
-</html>
