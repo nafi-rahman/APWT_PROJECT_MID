6cbdcd1b97a050074bfa683a37f9ce362340741b
nafi-rahman <nafirahman16@gmail.com>
2022-03-14 03:18:24 +0600

    userlist

--- Virtual_Meet/resources/views/pages/admin/profile.blade.php
+++ Virtual_Meet/resources/views/pages/admin/profile.blade.php
@@ -48,63 +48,70 @@
                                                     <input type="text" name="email" value="{{$user->email}}"
                                                         class="form-control">
                                                 </div>
-@extends('layouts.appAdmin')
-@section('contentAdmin')
-<br><br>
-     <h1>Profile</h1>
-    <form action= "{{route('profileAdminEdit')}}" class "form-group" method = "post" enctype="multipart/form-data">
-        @csrf
-        <div class="container">
-            <div class="row ">
-                <div class="form-group">
-                    <input type="text" name="id"  value = "{{$user->id}}" class="form-control" readonly>
-                </div>
-        <div class="form-group">
-                <input type="text" name="name"  value = "{{$user->name}}" class="form-control">
-            </div>
-            <br>
-            <div class="form-group">
-                <input type="text" name="password"  value = "{{$user->password}}" class="form-control">
-            </div>
-            <br>
-            <div class="form-group">
-                <input type="text" name="email" value = "{{$user->email}}" class="form-control">
-            </div>
+                                                @extends('layouts.appAdmin')
+                                                @section('contentAdmin')
+                                                <br><br>
+                                                <h1>Profile</h1>
+                                                <form action="{{route('profileAdminEdit')}}" class "form-group"
+                                                    method="post" enctype="multipart/form-data">
+                                                    @csrf
+                                                    <div class="container">
+                                                        <div class="row ">
+                                                            <div class="form-group">
+                                                                <input type="text" name="id" value="{{$user->id}}"
+                                                                    class="form-control" readonly>
+                                                            </div>
+                                                            <div class="form-group">
+                                                                <input type="text" name="name" value="{{$user->name}}"
+                                                                    class="form-control">
+                                                            </div>
+                                                            <br>
+                                                            <div class="form-group">
+                                                                <input type="text" name="password"
+                                                                    value="{{$user->password}}" class="form-control">
+                                                            </div>
+                                                            <br>
+                                                            <div class="form-group">
+                                                                <input type="text" name="email" value="{{$user->email}}"
+                                                                    class="form-control">
+                                                            </div>
 
-                                                <br>
-                                                <div class="form-group">
-                                                    <input type="text" name="phone" value="{{$user->phone}}"
-                                                        class="form-control">
-                                                </div>
-                                                <br>
-                                                <br>
+                                                            <br>
+                                                            <div class="form-group">
+                                                                <input type="text" name="phone" value="{{$user->phone}}"
+                                                                    class="form-control">
+                                                            </div>
+                                                            <br>
+                                                            <br>
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-md-3">
+
+
+                                                        <img class="img-thumbnail rounded mx-auto d-block"
+                                                            src="{{asset('images/'.$user->image_path)}}"
+                                                            alt="Card image cap">
+
+
+                                                        <div class="form-group">
+                                                            <button type="submit"
+                                                                class="btn btn-success">Update</button>
+                                                        </div>
+                                                    </div>
+                                                </form>
+
+
                                             </div>
-                                        </div>
-                                        <div class="col-md-3">
 
-
-                                                <img class="img-thumbnail rounded mx-auto d-block" src="{{asset('images/'.$user->image_path)}}"
-                                                    alt="Card image cap">
-
-
-                                            <div class="form-group">
-                                                <button type="submit" class="btn btn-success">Update</button>
+                                            <div>
+                                                {{-- @yield('footer') --}}
                                             </div>
+
                                         </div>
-                                    </form>
-
-
                                 </div>
-
-                                <div>
-                                    {{-- @yield('footer') --}}
-                                </div>
-
                             </div>
                         </div>
                     </div>
-                </div>
-            </div>
         </section>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
@@ -112,17 +119,17 @@
     </body>
 
     </html>
-            <br>
-            <br>
-        </div>
-        </div>
-        <div class="col-md-3">
+    <br>
+    <br>
+    </div>
+    </div>
+    <div class="col-md-3">
         <div class="card" style="width: 18rem;">
             <img class="img-thumbnail" src="{{asset('images/'.$user->image_path)}}" alt="Card image cap">
         </div>
         <div class="form-group">
             <button type="submit" class="btn btn-success">Update</button>
         </div>
-        </div>
+    </div>
     </form>
-@endsection
+    @endsection
